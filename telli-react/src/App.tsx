8ae5c5669774b1dde704a20c5ae802c46bdd7e18
Jessica <jlknobeloch@gmail.com>
2024-02-15 16:41:42 -0600

    created component for user registration form

--- telli-react/src/App.tsx
+++ telli-react/src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import CreateNewWatchListForm from './components/CreateNewWatchList'
-import DisplayAllWatchLists from './components/ViewWatchLists'
+import { React, useState } from "react";
+import "./App.css";
+import CreateNewWatchListForm from "./components/CreateNewWatchList";
+import DisplayAllWatchLists from "./components/ViewWatchLists";
+import RegistrationForm from "./components/RegistrationForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <CreateNewWatchListForm />
       <DisplayAllWatchLists />
+      <RegistrationForm />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
